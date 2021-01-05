163a81ed9cab9292e916a039c39cbc7e28cc8435
Noah Heague <noahheague@gmail.com>
2021-01-05 20:25:29 +1100

    Generic Ack

--- api/src/discord/discord.ts
+++ api/src/discord/discord.ts
@@ -2,6 +2,7 @@ import { APIGatewayEvent, Context, Callback } from 'aws-lambda';
 import { authorise } from './utils/auth';
 import { DiscordPing } from './ping/ping';
 import { newLogger } from './utils/logger';
+import { Interaction } from './utils/discord-types';
 
 export const handler = async (
   event: APIGatewayEvent,
@@ -27,6 +28,20 @@ export const handler = async (
     }
 
     logger.info(`Received command\n${JSON.stringify(event.body)}`);
+
+    const interaction: Interaction = JSON.parse(event.body as string);
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        type: 4,
+        data: {
+          tts: false,
+          content: `Command Received: ${interaction.data?.name} from ${interaction.member.user.username}!\nCommand Values:${interaction.data?.options?.map((option) => `\n**${option.name}:** *${option.value}*`)}`,
+          embeds: [],
+          allowed_mentions: [],
+        },
+      }),
+    });
   } catch (err) {
     logger.error(err);
 
